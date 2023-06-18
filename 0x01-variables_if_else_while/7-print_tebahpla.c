56018fd8a9724198f4a879dfd793e8f4bc6f1cde
nkazimuna <nkazimuna@gmail.com>
2023-06-18 03:21:19 -0700

    0x01. C - Variables, if, else, while Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdion.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}

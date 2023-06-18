c301e17f7e0b2af9fa07468ff1f19e1db0222cdb
nkazimuna <nkazimuna@gmail.com>
2023-06-18 03:33:49 -0700

    0x01. C - Variables, if, else, while Task 88

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+
+	return (0);
+}

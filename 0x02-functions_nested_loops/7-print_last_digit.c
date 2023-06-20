ba6ede771a78413bf41fc94645a0161fc9cd277d
nkazimuna <nkazimuna@gmail.com>
2023-06-20 05:23:43 -0700

    0x02. C - Functions, nested loops task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @a: user input
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int a)
+{
+	if (a < 0)
+	{
+		a = a * -a;
+		_puchar((a % 10) + '0');
+		return (n % 10);
+	}
+}

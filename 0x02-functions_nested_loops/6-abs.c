31689a4fbc0595e06ae9ef6419926b3b0ee7a0cf
nkazimuna <nkazimuna@gmail.com>
2023-06-20 04:56:03 -0700

    0x02. C - Functions, nested loops task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an inter
+ * @a: user input
+ * Return: will return given value if it is an integer
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = a + 1;
+	}
+	return (a);
+}

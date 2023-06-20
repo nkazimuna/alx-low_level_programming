a911f2d069c1b705af076c8f24189e5dc4d4987f
nkazimuna <nkazimuna@gmail.com>
2023-06-20 05:52:26 -0700

    0x02. C - Functions, nested loops task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer -Prints every minute of the day
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

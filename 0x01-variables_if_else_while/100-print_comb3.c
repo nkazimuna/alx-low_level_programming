1f96b1a1634c9bef3fde6cc122abc222a4b152e6
nkazimuna <nkazimuna@gmail.com>
2023-06-20 00:14:59 -0700

    0x01. C - Variables, if, else while task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,9 @@ int main(void)
 	int a, b;
 
 	for (a = 0; a < 9; a++)
+	{
 		for (b = a + 1; b < 10; b++)
+		{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
 
@@ -17,8 +19,8 @@ int main(void)
 
 			putchar(',');
 			putchar(' ');
-
-
+		}
+	}
 	putchar('\n');
 	return (0);
 }

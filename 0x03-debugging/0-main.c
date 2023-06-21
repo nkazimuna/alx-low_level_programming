0a02f5c35d7f6129c19d222d30dd4387bec2557b
nkazimuna <nkazimuna@gmail.com>
2023-06-20 23:46:27 -0700

    0x03. C - Debugging Initial task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Test if given value is positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	positive_or_negative(a);
+	
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0x03. C - Debugging project 21/06/2023 learning to debug code manually.

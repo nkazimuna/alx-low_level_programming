6b1aca49009139c516cd2ebdce2a98bb84bb5478
nkazimuna <nkazimuna@gmail.com>
2023-06-22 00:46:47 -0700

    0x04. C - More functions, more nested loops Task 2 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: input one
+ * @b: input two
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

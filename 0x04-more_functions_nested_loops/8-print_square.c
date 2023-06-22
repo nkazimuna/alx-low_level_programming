351a128e0b7ea9ed97cfbe181861bf33f597a7a6
nkazimuna <nkazimuna@gmail.com>
2023-06-22 04:05:32 -0700

    0x03. C - More functions, more nested loops Task 8 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - print a square followed by a new line
+ * @size: user input
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			_putchar('#');
+			for (b = 2; b <= size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

48f96be0d835d0b85a22cc33de71928658f83c52
nkazimuna <nkazimuna@gmail.com>
2023-06-26 04:19:55 -0700

    0x05. C - Pointers, arrays and string task 3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: input from user
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}

9db2906319a97ab13d56b2e44bd51437b9ab7c4a
nkazimuna <nkazimuna@gmail.com>
2023-06-26 04:36:11 -0700

    0x05. C - Pointers, arrays and strings Task 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new  line
+ * @s: user input
+ */
+
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (a = a - 1; s[a] != '\0'; a--)
+		{
+			_putchar(s[a]);
+		}
+	_putchar('\n');
+	}
+}
